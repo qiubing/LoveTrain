092c8307e090070d2b72d45658158a82269e14c1
LK <lk>
2015-09-01 09:47:16 +0800

    创建HttpUtil等工具类

--- app/src/main/java/cn/nubia/activity/LoginActivity.java
+++ app/src/main/java/cn/nubia/activity/LoginActivity.java
@@ -5,13 +5,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import cn.nubia.util.DialogUtil;
+import cn.nubia.util.HttpUtil;
 
 public class LoginActivity extends Activity implements View.OnClickListener {
 
     private Button loginButton;
     private TextView regist;
     private TextView forgetPwd;
+
+    private EditText etName ;
+    private EditText etPwd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +34,8 @@ public class LoginActivity extends Activity implements View.OnClickListener {
         loginButton = (Button) findViewById(R.id.login_btn);
         regist = (TextView) findViewById(R.id.login_regist);
         forgetPwd = (TextView) findViewById(R.id.login_forgetpwd);
+        etName = (EditText) findViewById(R.id.login_name);
+        etPwd = (EditText) findViewById(R.id.login_pwd);
 
         loginButton.setOnClickListener(this);
         regist.setOnClickListener(this);
@@ -30,17 +46,47 @@ public class LoginActivity extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         Intent intent = null;
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.login_btn:
                 break;
             case R.id.login_regist:
-                intent = new Intent(this,RegistActivity.class);
+                intent = new Intent(this, RegistActivity.class);
                 startActivity(intent);
                 break;
             case R.id.login_forgetpwd:
+                DialogUtil.showDialog(this, "forget", false);
                 break;
             default:
                 break;
         }
+    }
+
+    private boolean login(){
+        String username = etName.getText().toString();
+        String pwd = etPwd.getText().toString();
+        return false;
+    }
+
+    private JSONObject query(String username,String pwd) throws JSONException {
+        Map<String,String> map = new HashMap<>();
+        map.put("username",username);
+        map.put("password",pwd);
+        String url = "";
+        return new JSONObject((HttpUtil.postRequest(url,"","")));
+    }
+
+    //对用户名和密码进行校验
+    public boolean validate(){
+        String username = etName.getText().toString().trim();
+        if(username.equals("")){
+            DialogUtil.showDialog(this,"用户名不能为空",false);
+            return  false;
+        }
+        String pwd = etPwd.getText().toString().trim();
+        if(pwd.equals("")){
+            DialogUtil.showDialog(this,"密码不能为空",false);
+            return false;
+        }
+        return true;
     }
 }
