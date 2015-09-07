cf28af478fd1591274106e7eeaf453d96a8c1d11
hulijia <hulijia@>
2015-09-07 17:32:28 +0800

    配置文件

--- app/src/main/java/cn/nubia/activity/admin/MyAdminActivity_1.java
+++ app/src/main/java/cn/nubia/activity/admin/MyAdminActivity_1.java
@@ -1,18 +1,17 @@
 package cn.nubia.activity.admin;
 
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import cn.nubia.activity.R;
 
-/**
- * Created by 胡立 on 2015/9/7.
- */
+
 public class MyAdminActivity_1 extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "MyAdminActivity_1", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "CourseAdminActivity_1", Toast.LENGTH_LONG).show();
     }
 }
