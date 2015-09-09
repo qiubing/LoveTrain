fbc1f76e4353bb55f1ec14cdf659254e673a8e49
hexiao <hx@qq.com>
2015-09-09 11:05:20 +0800

    hx

--- app/src/main/java/cn/nubia/activity/admin/AdminAlterCourseActivity.java
+++ app/src/main/java/cn/nubia/activity/admin/AdminAlterCourseActivity.java
@@ -1,6 +1,9 @@
 package cn.nubia.activity.admin;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -16,7 +19,7 @@ import cn.nubia.activity.R;
 /**
  * Created by hexiao on 2015/9/7.
  */
-public class AdminAlterCourseActivity extends Activity implements View.OnClickListener{
+public class AdminAlterCourseActivity extends Activity implements View.OnClickListener {
     private EditText alterCourseCourseNameEditText;
     private EditText alterCourseCourseDescEditText;
 
@@ -35,13 +38,13 @@ public class AdminAlterCourseActivity extends Activity implements View.OnClickLi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_alter_course);
-        alterCourseCourseNameEditText=(EditText)findViewById(R.id.alter_course_courseName_editText);
-        alterCourseCourseDescEditText=(EditText)findViewById(R.id.alter_course_courseDesc_editText);
-        alterCourseButton=(Button)findViewById(R.id.alter_course_button);
-        alterCourseBackImage=(ImageView)findViewById(R.id.admin_alter_course_backImage);
+        alterCourseCourseNameEditText = (EditText) findViewById(R.id.alter_course_courseName_editText);
+        alterCourseCourseDescEditText = (EditText) findViewById(R.id.alter_course_courseDesc_editText);
+        alterCourseButton = (Button) findViewById(R.id.alter_course_button);
+        alterCourseBackImage = (ImageView) findViewById(R.id.admin_alter_course_backImage);
 
-        alterCourseWhetherExamCheckBox=(CheckBox)findViewById(R.id.alter_course_whetherExam_checkBox);
-        alterCourseWhetherHighLevelCourseCheckBox=(CheckBox)findViewById(R.id.alter_course_whetherHighLevelCourse_checkBox);
+        alterCourseWhetherExamCheckBox = (CheckBox) findViewById(R.id.alter_course_whetherExam_checkBox);
+        alterCourseWhetherHighLevelCourseCheckBox = (CheckBox) findViewById(R.id.alter_course_whetherHighLevelCourse_checkBox);
 
         alterCourseButton.setOnClickListener(this);
         alterCourseBackImage.setOnClickListener(this);
@@ -49,24 +52,43 @@ public class AdminAlterCourseActivity extends Activity implements View.OnClickLi
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.admin_alter_course_backImage:
                 Toast.makeText(AdminAlterCourseActivity.this, "你点击了返回", Toast.LENGTH_LONG).show();
-                Intent intentBackImage = new Intent(AdminAlterCourseActivity.this,MainAdminActivity.class);
+                Intent intentBackImage = new Intent(AdminAlterCourseActivity.this, MainAdminActivity.class);
                 startActivity(intentBackImage);
                 finish();
                 break;
             case R.id.alter_course_button:
-                String courseName=alterCourseCourseNameEditText.getText().toString();
-                String courseDesc=alterCourseCourseDescEditText.getText().toString();
-                whetherExam=alterCourseWhetherExamCheckBox.isChecked();
-                whetherHighLevelCourse=alterCourseWhetherHighLevelCourseCheckBox.isChecked();
-                //加入到课程数据库中，返回是否加入成功的状态值
-                //....
-                Intent intentAlterForSure = new Intent(AdminAlterCourseActivity.this,AdminCourseDetailActivity.class);
-                startActivity(intentAlterForSure);
-                Toast.makeText(AdminAlterCourseActivity.this, "你点击了确认修改", Toast.LENGTH_LONG).show();
-                finish();
+                Dialog alterCourseDialog = new AlertDialog.Builder(AdminAlterCourseActivity.this)
+                        .setTitle("修改课程")
+                        .setMessage("确定修改？")
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                String courseName = alterCourseCourseNameEditText.getText().toString();
+                                String courseDesc = alterCourseCourseDescEditText.getText().toString();
+                                whetherExam = alterCourseWhetherExamCheckBox.isChecked();
+                                whetherHighLevelCourse = alterCourseWhetherHighLevelCourseCheckBox.isChecked();
+                                //加入到课程数据库中，返回是否加入成功的状态值
+                                //....
+                                Intent intentAlterForSure = new Intent(AdminAlterCourseActivity.this, AdminCourseDetailActivity.class);
+                                startActivity(intentAlterForSure);
+                                //这里执行修改课程操作
+                                Toast.makeText(AdminAlterCourseActivity.this, "你点击了确认修改", Toast.LENGTH_LONG).show();
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent intentAlterCancel = new Intent(AdminAlterCourseActivity.this, AdminCourseDetailActivity.class);
+                                startActivity(intentAlterCancel);
+                            }
+                        })
+                        .create();
+                alterCourseDialog.show();
                 break;
         }
 
