2f1df2fe3d8bdda68bc1f86c713b22cb0a1f0b7f
LK <lk>
2015-09-11 15:31:36 +0800

    award credits

--- app/src/main/java/cn/nubia/activity/admin/AdminCreditsAwardActivity.java
+++ app/src/main/java/cn/nubia/activity/admin/AdminCreditsAwardActivity.java
@@ -2,74 +2,77 @@ package cn.nubia.activity.admin;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.RatingBar;
-import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import cn.nubia.activity.R;
 import cn.nubia.entity.CreditsAwardMsg;
-import cn.nubia.entity.LessonJudgement;
 
 /**
  * Created by JiangYu on 2015/9/6.
  */
-public class AdminCreditsAwardActivity extends Activity{
+public class AdminCreditsAwardActivity extends Activity {
     private Button mConfirmButton;
-    private ImageView mBackButton;
     private EditText mAwardedName;
     private EditText mAwardCredits;
     private EditText mAwardCause;
+
+    private TextView mManagerTitle;
+    private ImageView mGoBack;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_credits_award);
 
+        //公用部分
+        mManagerTitle = (TextView) findViewById(R.id.manager_head_title);
+        mManagerTitle.setText(R.string.activity_manager_award_title);
+        mGoBack = (ImageView) findViewById(R.id.manager_goback);
+        mGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
         holdView();
         setViewLogic();
     }
 
-    private void holdView(){
-        mConfirmButton =(Button) findViewById(
+    private void holdView() {
+        mConfirmButton = (Button) findViewById(
                 R.id.activity_manager_award_confirm_button);
-        mBackButton =(ImageView) findViewById(
-                R.id.manager_award_backImage);
-        mAwardedName =(EditText) findViewById(
+        mAwardedName = (EditText) findViewById(
                 R.id.activity_manager_awarded_name);
-        mAwardCredits =(EditText) findViewById(
+        mAwardCredits = (EditText) findViewById(
                 R.id.activity_manager_awarded_credit);
-        mAwardCause =(EditText) findViewById(
+        mAwardCause = (EditText) findViewById(
                 R.id.activity_manager_awarded_cause);
     }
 
-    private void setViewLogic(){
+    private void setViewLogic() {
         /**监听确认按钮，进行提交动作*/
-        mConfirmButton.setOnClickListener(makeConfirmOnClickListener());
-        mBackButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AdminCreditsAwardActivity.this.finish();
-            }
-        });
+
     }
 
-    private boolean checkData(){
-        if(mAwardedName.getText().toString()==null){
+    private boolean checkData() {
+        if (mAwardedName.getText().toString() == null) {
             Toast.makeText(this, "被奖励人姓名不能为空", Toast.LENGTH_SHORT).show();
             return false;
         }
-        if(mAwardCredits.getText().toString()==null){
+        if (mAwardCredits.getText().toString() == null) {
             Toast.makeText(this, "奖励积分不能为空", Toast.LENGTH_SHORT).show();
             return false;
         }
         return true;
     }
 
-    private View.OnClickListener makeConfirmOnClickListener(){
+    private View.OnClickListener makeConfirmOnClickListener() {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
