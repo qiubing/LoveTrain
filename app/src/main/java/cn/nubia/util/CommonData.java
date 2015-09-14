1ac9103caa29a93a2563dd0c45766dfe49c5f63b
LK <lk>
2015-09-14 15:28:46 +0800

    common style

---
+++ app/src/main/java/cn/nubia/util/CommonData.java
@@ -0,0 +1,10 @@
+package cn.nubia.util;
+
+/**
+ * Created by LK on 2015/9/14.
+ * 缓存类,全局缓存
+ */
+public class CommonData {
+    public static String DEVIDE_ID = null;
+    public static String versionCode = null;
+}
