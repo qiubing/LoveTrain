f51ccaf131dab882057bf13d786e2ec8c0e148a9
hulijia <hulijia@>
2015-09-21 21:20:45 +0800

    时间转换类

---
+++ app/src/main/java/cn/nubia/util/jsonprocessor/TimeFormatConversion.java
@@ -0,0 +1,27 @@
+package cn.nubia.util.jsonprocessor;
+
+import java.util.Calendar;
+
+/**
+ * Created by 胡立 on 2015/9/21.
+ */
+public class TimeFormatConversion {
+    public static String toTimeDate(long time) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(time);
+        return calendar.get(Calendar.YEAR) + "-" + (calendar.get(Calendar.MONTH) + 1) + "-" +
+                calendar.get(Calendar.DAY_OF_MONTH);
+    }
+
+    public static String toTime(long time) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(time);
+        return calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.MINUTE);
+    }
+
+    public static long toTimeInMillis(int year, int month, int day, int hour, int minute) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day, hour, minute);
+        return calendar.getTimeInMillis();
+    }
+}
