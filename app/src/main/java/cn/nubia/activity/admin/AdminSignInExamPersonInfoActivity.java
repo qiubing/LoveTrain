4d79e1dfaafdb21634c8f6db0db2455167d4f98a
hulijia <hulijia@>
2015-10-08 18:21:12 +0800

    转圈圈

--- app/src/main/java/cn/nubia/activity/admin/AdminSignInExamPersonInfoActivity.java
+++ app/src/main/java/cn/nubia/activity/admin/AdminSignInExamPersonInfoActivity.java
@@ -1,13 +1,22 @@
 package cn.nubia.activity.admin;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
@@ -23,14 +32,21 @@ import java.util.Map;
 import cn.nubia.activity.R;
 import cn.nubia.entity.Constant;
 import cn.nubia.entity.ExamItem;
+import cn.nubia.interfaces.IOnGestureListener;
 import cn.nubia.util.AsyncHttpHelper;
+import cn.nubia.util.GestureDetectorManager;
 
 /**
  * Created by hexiao on 2015/9/8.
  */
-public class AdminSignInExamPersonInfoActivity extends Activity implements View.OnClickListener{
+public class AdminSignInExamPersonInfoActivity extends Activity{
     private ExamItem mExamItem;
     private final static String URL = Constant.BASE_URL +"exam/exam_people_list.do";
+    private RelativeLayout loadingFailedRelativeLayout;
+    private RelativeLayout networkUnusableRelativeLayout;
+    private GestureDetector gestureDetector;
+    private ImageView loadingView;
+    private RotateAnimation refreshingAnimation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,24 +58,58 @@ public class AdminSignInExamPersonInfoActivity extends Activity implements View.
     }
 
     private void initView(){
-        TextView barTxt = (TextView) findViewById(R.id.manager_head_title);
+        TextView barTxt = (TextView) findViewById(R.id.sub_page_title);
         barTxt.setText("报名考试人数");
+        loadingFailedRelativeLayout = (RelativeLayout) findViewById(R.id.loading_failed);
+        networkUnusableRelativeLayout = (RelativeLayout) findViewById(R.id.network_unusable);
+        loadingFailedRelativeLayout.setVisibility(View.GONE);
+        networkUnusableRelativeLayout.setVisibility(View.GONE);
+        networkUnusableRelativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Settings.ACTION_WIFI_SETTINGS);
+                Log.i("huhu", "ClientEvaluateActivity onClick");
+                startActivity(intent);
+            }
+        });
+        GestureDetectorManager gestureDetectorManager = GestureDetectorManager.getInstance();
+        //指定Context和实际识别相应手势操作的GestureDetector.OnGestureListener类
+        gestureDetector = new GestureDetector(this, gestureDetectorManager);
+
+        //传入实现了IOnGestureListener接口的匿名内部类对象，此处为多态
+        gestureDetectorManager.setOnGestureListener(new IOnGestureListener() {
+            @Override
+            public void finishActivity() {
+                finish();
+            }
+        });
+    }
+
+    //将Activity上的触碰事件交给GestureDetector处理
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent motionEvent){
+        super.dispatchTouchEvent(motionEvent); //让Activity响应触碰事件
+        gestureDetector.onTouchEvent(motionEvent); //让GestureDetector响应触碰事件
+        return false;
     }
 
     private void initEvent(){
         mExamItem = (ExamItem) getIntent().getSerializableExtra("ExamIndex");
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.manager_goback:
-                finish();
-                break;
-        }
-    }
 
     private void loadData(){
+        loadingFailedRelativeLayout.setVisibility(View.GONE);
+        networkUnusableRelativeLayout.setVisibility(View.GONE);
+
+        loadingView = (ImageView)findViewById(R.id.loading_iv);
+        loadingView.setVisibility(View.VISIBLE);
+        refreshingAnimation = (RotateAnimation) AnimationUtils.loadAnimation(this, R.anim.rotating);
+        //添加匀速转动动画
+        LinearInterpolator lir = new LinearInterpolator();
+        refreshingAnimation.setInterpolator(lir);
+        loadingView.startAnimation(refreshingAnimation);
+
         RequestParams requestParams = new RequestParams(Constant.getRequestParams());
         requestParams.put("exam_index", mExamItem.getIndex());
         AsyncHttpHelper.post(URL, requestParams, myJsonHttpResponseHandler);
@@ -68,10 +118,12 @@ public class AdminSignInExamPersonInfoActivity extends Activity implements View.
     private final JsonHttpResponseHandler myJsonHttpResponseHandler = new JsonHttpResponseHandler(){
         @Override
         public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-            Log.e("wj","signExamPerson"+response.toString());
+            loadingView.clearAnimation();
+            loadingView.setVisibility(View.GONE);
 
             try {
                 if(response.getInt("code") != 0){
+                    loadingFailedRelativeLayout.setVisibility(View.VISIBLE);
                     return;
                 }
                 JSONArray jsonArray = response.getJSONArray("data");
@@ -97,13 +149,17 @@ public class AdminSignInExamPersonInfoActivity extends Activity implements View.
 
             } catch (JSONException e) {
                 e.printStackTrace();
+                loadingFailedRelativeLayout.setVisibility(View.VISIBLE);
             }
         }
 
         @Override
         public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
             super.onFailure(statusCode, headers, throwable, errorResponse);
-            Log.e("test","onFailure");
+            networkUnusableRelativeLayout.setVisibility(View.VISIBLE);
+            Toast.makeText(AdminSignInExamPersonInfoActivity.this, "网络没有连接，请连接网络", Toast.LENGTH_SHORT).show();
+            loadingView.clearAnimation();
+            loadingView.setVisibility(View.GONE);
         }
     };
 
